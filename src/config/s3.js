7207308c361dddb4bf5609fff7fac34e33798a71
manyakukreja1306 <manyakukreja2005@gmail.com>
2026-08-05 00:38:29 +0530

    feat: upgrade S3 to AWS SDK v3 with presigned URL support

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,17 +1,33 @@
-const AWS = require("aws-sdk");
+const { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: process.env.AWS_REGION
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION || "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
 });
 
-exports.uploadFile = async (file, key) => {
-  const params = {
-    Bucket: process.env.S3_BUCKET,
-    Key: key,
-    Body: file.buffer
-  };
+const BUCKET = process.env.S3_BUCKET;
 
-  return s3.upload(params).promise();
+exports.uploadFile = async (fileBuffer, mimeType, s3Key) => {
+  const command = new PutObjectCommand({
+    Bucket: BUCKET,
+    Key: s3Key,
+    Body: fileBuffer,
+    ContentType: mimeType,
+  });
+  await s3Client.send(command);
+  return s3Key;
+};
+
+exports.generatePresignedUrl = async (s3Key, expiresInSeconds = 900) => {
+  const command = new GetObjectCommand({ Bucket: BUCKET, Key: s3Key });
+  return getSignedUrl(s3Client, command, { expiresIn: expiresInSeconds });
+};
+
+exports.deleteObject = async (s3Key) => {
+  const command = new DeleteObjectCommand({ Bucket: BUCKET, Key: s3Key });
+  await s3Client.send(command);
 };
